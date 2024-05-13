30213b9a1d87e957a62caa84d603eff00b715b2b
fn78 <oh.seyy@yahoo.de>
2024-05-13 03:39:41 +0200

    some time adjustments

--- main.js
+++ main.js
@@ -4,6 +4,8 @@
 
 var date = new Date();
 var serverDate;
+var isSummerTime = true;
+var serverTimeOffset = 2;
 
 var userTimezoneOffset = date.getTimezoneOffset() * 60000;
 
@@ -17,6 +19,12 @@ var ui = {
 function updateTime() {
     date = new Date();
     serverDate = new Date(date.getTime() + userTimezoneOffset + (2 * 60 * 60 * 1000));
+    // ^change 2 to play with the time
+
+    isSummerTime = serverDate.getMonth() >= 3 && serverDate.getMonth() < 10;
+    serverTimeOffset = isSummerTime ? 2 : 1;
+
+    // ignore this:
     //now = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours() + 2, date.getUTCMinutes(), date.getUTCSeconds()));
     // this instead of new date to account for +2 time zone
 }
@@ -24,13 +32,13 @@ function updateTime() {
 function calculateGC() {
     let text = "";
 
-    let isGC = Math.floor(((serverDate.getTime() + (2 * 60 * 60 * 1000)) / 1000 / 60 / 60 / 24) - 1) % 2;
+    let isGC = (Math.floor(((serverDate.getTime() + userTimezoneOffset) / 1000 / 60 / 60) + 2) / 24) % 2;
     text = "Today is " + weekdays[serverDate.getDay()] + "."
-        + "<br />Today the Global Challenge is <span style='font-size:32px; color:" + (isGC ? "lightgreen" : "red") + "'>" + (isGC ? "ACTIVE" : "INACTIVE") + "</span>!"
+        + "<br />Today the Global Challenge is <span style='font-size:32px; color:" + (isGC ? "lightgreen" : "red") + "'>" + (isGC ? "ACTIVE!" : "INACTIVE!") + "</span>"
         + (isGC ? ("<br />It started " + serverDate.getHours() + " hours and " + serverDate.getMinutes() + " minutes ago.") :
         ("<br />It will start in " + (23 - serverDate.getHours()) + " hours and " + (59 - serverDate.getMinutes()) + " minutes."))
-        + "<br /><br /><br />The server time is " + serverDate.toString().split(" GMT")[0] + "."
-        + "<br />Reset is always at 22: 00 GMT + 0. (11 pm or midnight in Germany)";
+        + "<br /><br /><br />The server time is " + serverDate.toString().split(" GMT")[0] + " (GMT+" + serverTimeOffset + ")."
+        + "<br />Reset is always at 22:00 GMT + 0. (Currently " + (isSummerTime ? "midnight" : "11 P.M.") + " for Germany/the server)";
 
     ui.textGC.innerHTML = text;
 }
