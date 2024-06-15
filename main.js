fe5a54abbb18924f6fc6a2f44cf098492b361924
fn78 <oh.seyy@yahoo.de>
2024-06-15 21:22:31 +0200

    CombCalc v1.1

--- main.js
+++ main.js
@@ -17,7 +17,25 @@ var hourDelay = 0;
 
 var ui = {
     textGC: document.getElementById("textGC"),
+    textCGC: document.getElementById("textCGC"),
+
+    combinesLeft: document.getElementById("combinesLeft"),
+    combinesRight: document.getElementById("combinesRight"),
+
+    moreTokensLevel: document.getElementById("moreTokensLevel"),
+    moreTokensLevel2: document.getElementById("moreTokensLevel2"),
+    progressGlobal: document.getElementById("progressGlobal"),
+    progressYours: document.getElementById("progressYours"),
+    progressGlobalText: document.getElementById("progressGlobalText"),
+    progressYoursText: document.getElementById("progressYoursText"),
 }
+
+ui.moreTokensLevel.oninput = () => { ui.moreTokensLevel2.value = "" }
+ui.moreTokensLevel2.oninput = () => { ui.moreTokensLevel.value = "" }
+ui.progressGlobal.oninput = () => { ui.progressGlobalText.innerHTML = "x" + ui.progressGlobal.value }
+ui.progressYours.oninput = () => { ui.progressYoursText.innerHTML = "x" + ui.progressYours.value }
+ui.progressGlobal.value = "5";
+ui.progressYours.value = "5";
 
 function updateTime() {
     date = new Date();
@@ -35,7 +53,6 @@ function updateTime() {
 function calculateGC() {
     let text = "";
 
-    // I C U cubbie, send me anime girlz
     let isGC = (Math.floor((date.getTime() / 1000 / 60 / 60 + (2 - hourDelay)) / 24) - 1) % 2;
     text = "Today is " + weekdays[serverDate.getDay()] + "."
         + "<br />Today the Global Challenge is <span style='font-size:32px; color:" + (isGC ? "lightgreen" : "red") + "'>" + (isGC ? "ACTIVE!" : "INACTIVE!") + "</span>"
@@ -47,11 +64,39 @@ function calculateGC() {
     ui.textGC.innerHTML = text;
 }
 
+function calculateCGC() {
+    let combinesEarned = ui.progressGlobal.value * ui.progressYours.value;
+    let combineMulti = 1;
+
+    if (ui.moreTokensLevel.value != "") {
+        combineMulti = 1 + (ui.moreTokensLevel.value / 10);
+    }
+    if (ui.moreTokensLevel2.value != "") {
+        if (ui.moreTokensLevel2.value >= 500) combineMulti = 2 + Math.max(0, Math.floor((ui.moreTokensLevel2.value - 1000) / 1000) / 10);
+        else combineMulti = 1;
+    }
+
+    combinesEarned *= combineMulti;
+
+    ui.textCGC.innerHTML = "You will earn <big>" + Math.round(combinesEarned) + " <img src='combineToken.png' style='width: 32px' />!</big>";
+
+    let sidesSize = 120;
+
+    ui.combinesLeft.style.height = "";
+    ui.combinesRight.style.height = "";
+    if (ui.combinesLeft.clientHeight > sidesSize) sidesSize = ui.combinesLeft.clientHeight;
+    if (ui.combinesRight.clientHeight > sidesSize) sidesSize = ui.combinesRight.clientHeight;
+
+    ui.combinesLeft.style.height = sidesSize + "px";
+    ui.combinesRight.style.height = sidesSize + "px";
+}
+
 function loop() {
     updateTime();
 
     // UI update
     calculateGC();
+    calculateCGC();
 }
 
 console.log("onions are literally a mass torture device");
