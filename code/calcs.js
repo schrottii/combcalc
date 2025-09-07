f3453f0c15f6c93bf3b21bd3872869dbb0560e44
fn78 <oh.seyy@yahoo.de>
2025-09-07 18:07:47 +0200

    v1.5: achievement boost calc, design rework

---
+++ code/calcs.js
@@ -0,0 +1,160 @@
+/* --------------------------------
+ *   COMBINE GAIN CALC
+ * ----------------------------- */
+
+function calculateGlobalChallengeTime() {
+    // Status: Is Global Challenge running / when / how long
+
+    let text = "";
+
+    let isGC = (Math.floor((date.getTime() / 1000 / 60 / 60 + (2 - hourDelay)) / 24) - 1) % 2;
+    text = "Today is " + weekdays[serverDate.getDay()] + "."
+        + "<br />Today the Global Challenge is <span style='font-size:32px; color:" + (isGC ? "lightgreen" : "red") + "'>" + (isGC ? "ACTIVE!" : "INACTIVE!") + "</span>"
+        + (isGC ? ("<br />It started " + serverMidnight.getHours() + " hours and " + serverMidnight.getMinutes() + " minutes ago.") :
+            ("<br />It will start in " + (23 - serverMidnight.getHours()) + " hours and " + (59 - serverMidnight.getMinutes()) + " minutes."))
+        + "<br /><br />The server time is " + serverDate.toString().split(" GMT")[0] + " (GMT+" + serverTimeOffset + ")."
+        + "<br />Reset is always at 22:00 GMT+0. (Currently " + (isSummerTime ? "0:00" : "23:00") + " for Germany/the server)";
+
+    ui.globalChallengeStatus.statusText.innerHTML = text;
+}
+
+function calculateGlobalChallenge() {
+    // Calculator: How many Combine Tokens will I earn
+
+    let combinesEarned = ui.combineGainCalc.progressGlobal.value * ui.combineGainCalc.progressYours.value;
+    let combineMulti = 1;
+
+    if (ui.combineGainCalc.moreTokensLevel.value != "") {
+        combineMulti = 1 + (ui.combineGainCalc.moreTokensLevel.value / 10);
+    }
+    if (ui.combineGainCalc.moreTokensLevel2.value != "") {
+        if (ui.combineGainCalc.moreTokensLevel2.value >= 500) combineMulti = 2 + Math.max(0, Math.floor((ui.combineGainCalc.moreTokensLevel2.value - 1000) / 1000) / 10);
+        else combineMulti = 1;
+    }
+
+    combinesEarned *= combineMulti;
+
+    ui.combineGainCalc.statusText.innerHTML = "You will earn <big>" + Math.floor(combinesEarned) + " <img src='images/combineToken.png' style='width: 32px' />!</big>";
+
+    let sidesSize = 120;
+
+    ui.combineGainCalc.combinesLeft.style.height = "";
+    ui.combineGainCalc.combinesRight.style.height = "";
+    if (ui.combineGainCalc.combinesLeft.clientHeight > sidesSize) sidesSize = ui.combineGainCalc.combinesLeft.clientHeight;
+    if (ui.combineGainCalc.combinesRight.clientHeight > sidesSize) sidesSize = ui.combineGainCalc.combinesRight.clientHeight;
+
+    ui.combineGainCalc.combinesLeft.style.height = sidesSize + "px";
+    ui.combineGainCalc.combinesRight.style.height = sidesSize + "px";
+}
+
+/* --------------------------------
+ *   MORE SCRAP CALC
+ * ----------------------------- */
+
+function calculateMoreScrap() {
+    // Calculator: What should my More Scrap level be
+
+    let highestScrapEver = ui.moreScrapCalc.highestScrapEver.value != "" ? new Decimal(ui.moreScrapCalc.highestScrapEver.value) : -1;
+    let moreGSLevel = ui.moreScrapCalc.moreGSLevel.value != "" ? ui.moreScrapCalc.moreGSLevel.value : -1;
+    if (highestScrapEver == -1 || moreGSLevel == -1) {
+        ui.moreScrapCalc.statusText.innerHTML = "Please enter your numbers";
+        return false;
+    }
+
+    let a = Math.log10(1.4) / highestScrapEver.log(10);
+    let result = ((1 / (2 * a)) * -1) + Math.sqrt(Math.pow(moreGSLevel, 2) + (moreGSLevel / 0.03) + (1 / (4 * Math.pow(a, 2))));
+
+    ui.moreScrapCalc.statusText.innerHTML = "a: " + a.toFixed(6) + ". <b>Your More Scrap (Book upgrade) should be level " + Math.floor(result) + "!</b>";
+}
+
+/* --------------------------------
+ *   TOKEN COST CALC
+ * ----------------------------- */
+
+var adNames = ["automerge", "bricks", "moremagnets", "morescrap", "fasterbarrels", "flu", "morefragments"];
+const adCosts = {
+    "automerge": [0, 1, 5, 1, 12, 8, 1],
+    "bricks": [1, 0, 1, 1, 1, 1, 1],
+    "moremagnets": [5, 1, 0, 1, 8, 5, 1],
+    "morescrap": [1, 1, 1, 0, 1, 1, 1],
+    "fasterbarrels": [12, 1, 8, 1, 0, 8, 5],
+    "flu": [8, 1, 5, 1, 8, 0, 1],
+    "morefragments": [1, 1, 1, 1, 5, 1, 0]
+}
+
+function calculateTokenCosts() {
+    let costs = 0;
+    let ad1 = ui.tokenCostCalc.selectedAd1.value;
+    let ad2 = ui.tokenCostCalc.selectedAd2.value;
+
+    if (ad1 == "none" || ad2 == "none") {
+        // one of the two ads is not selected
+        ui.tokenCostCalc.statusText.innerHTML = "(Select the two ads you want to combine!)";
+    }
+    else if (ad1 == ad2) {
+        // you want x100 barrels huh?!
+        ui.tokenCostCalc.statusText.innerHTML = "(Don't select the same boost twice)";
+    }
+    else {
+        // both ARE selected
+        costs = adCosts[ad1][adNames.indexOf(ad2)];
+
+        if (ui.tokenCostCalc.tokenCostAmountOfTokens.value != "0") {
+            // how many tokens to spend
+            let times = Math.floor(ui.tokenCostCalc.tokenCostAmountOfTokens.value / costs);
+            costs = costs * times;
+            ui.tokenCostCalc.statusText.innerHTML = times + " Times (" + costs + " Token" + (costs > 1 ? "s" : "") + ")";
+        }
+        else {
+            // how many times to combine
+            costs *= ui.tokenCostCalc.tokenCostAmountOfAds.value;
+            ui.tokenCostCalc.statusText.innerHTML = costs + " Token" + (costs > 1 ? "s" : "");
+        }
+    }
+}
+
+/* --------------------------------
+ *   BARREL PRODUCTION CALC
+ * ----------------------------- */
+
+function calculateBarrelProduction() {
+    let baseProd = ui.barrelProductionCalc.productionBarrelOne.value; // received as a string
+    try {
+        baseProd = new Decimal(baseProd);
+    }
+    catch {
+        ui.barrelProductionCalc.statusText.innerHTML = "Wrong format! Do it like this: 1.567e102 or 1 or 1.749e400";
+        // alert("Wrong format! Do it like this: 1.567e102 or 1 or 1.749e400");
+        return false;
+    }
+
+    let barrelnr = ui.barrelProductionCalc.barrelNumber.value;
+
+    // 3 ^ barrel x base
+    let result = new Decimal(3).pow(barrelnr - 1).mul(baseProd);
+    result = result.mantissa.toString().substr(0, 5) + "e" + result.exponent.toString();
+
+    if (barrelnr == "" || barrelnr < 1) ui.barrelProductionCalc.statusText.innerHTML = "That is not a real barrel!";
+    else if (baseProd.eq(1)) ui.barrelProductionCalc.statusText.innerHTML = "Barrel " + barrelnr + "'s base production is " + result + " Scrap/s.";
+    else ui.barrelProductionCalc.statusText.innerHTML = "Your barrel " + barrelnr + " should produce " + result + " Scrap/s.";
+}
+
+/* --------------------------------
+ *   ACHIEVEMENT BOOST CALC
+ * ----------------------------- */
+
+function calculateAchievementBoost() {
+    let levelDiff = parseInt(ui.achievementBoostCalc.goal.value) - parseInt(ui.achievementBoostCalc.start.value);
+
+    if (levelDiff > 0) {
+        let crystals = 0;
+        for (let l = parseInt(ui.achievementBoostCalc.start.value); l < parseInt(ui.achievementBoostCalc.goal.value); l++) {
+            crystals += l - 49;
+        }
+
+        ui.achievementBoostCalc.statusText.innerHTML = levelDiff + " level" + (levelDiff > 1 ? "s" : "") + ", " + crystals + " Crystal" + (crystals > 1 ? "s" : "");
+    }
+    else {
+        ui.achievementBoostCalc.statusText.innerHTML = "Invalid levels!";
+    }
+}
--- code/main.js
+++ code/main.js
@@ -15,12 +15,31 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 
 const FPS = 15;
 
-var currentVersion = "v1.4.1";
+var currentVersion = "v1.5";
+var currentVersionDate = "(2025-09-07)";
 var patchNotes = `
-- Added more invalid input texts (when you enter something illegal)
-- Added some placeholder texts (gray) to indicate what you are meant to enter
-- Fixed Summer time issue
-- Minor improvements
+-> New calc: Achievement Boost Calc
+- Calculates how many Crystals upgrading costs
+- Insert start level and goal level
+
+-> Design:
+- Boxes get ligher and wider when hovered over
+- Changed box border
+- Changed some colors (less flashy whites)
+- Changed design of the left-right green thing (and on mobile it's below now)
+- Smaller title (Schrottii's CombCalc)
+- Patch notes aligned to the left to be easier to read
+
+-> Images:
+- Added background image
+- New image for Barrel Production Calc
+- Moved calc images more to the left and made them a bit bigger
+
+-> Other:
+- More Scrap Calc: highest scrap ever is now inserted without log (ie 1e100 instead of 100)
+- Slightly changed some texts to be easier to understand
+- Slightly improved performance
+- Code improvements & re-organizing
 `;
 
 function updatePatchNotes() {
@@ -34,6 +53,7 @@ function updatePatchNotes() {
     }
 
     ui.bottom.patchNotes.innerHTML = render;
+    ui.bottom.currentVersion.innerHTML = currentVersion + " " + currentVersionDate;
 }
 
 // UI DICT
@@ -50,28 +70,35 @@ var ui = {
         progressGlobal: document.getElementById("progressGlobal"),
         progressYours: document.getElementById("progressYours"),
         progressGlobalText: document.getElementById("progressGlobalText"),
-        progressYoursText: document.getElementById("progressYoursText"),
+        progressYoursText: document.getElementById("progressYoursText")
     },
     moreScrapCalc: {
         statusText: document.getElementById("textMSC"),
         moreGSLevel: document.getElementById("moreGSLevel"),
-        highestScrapEver: document.getElementById("highestScrapEver"),
+        highestScrapEver: document.getElementById("highestScrapEver")
     },
     tokenCostCalc: {
         statusText: document.getElementById("textTCC"),
         selectedAd1: document.getElementById("combination1-select"),
         selectedAd2: document.getElementById("combination2-select"),
         tokenCostAmountOfAds: document.getElementById("tokenCostAmountOfAds"),
-        tokenCostAmountOfTokens: document.getElementById("tokenCostAmountOfTokens"),
+        tokenCostAmountOfTokens: document.getElementById("tokenCostAmountOfTokens")
     },
     barrelProductionCalc: {
         statusText: document.getElementById("textBPC"),
         productionBarrelOne: document.getElementById("bpcFirstProd"),
-        barrelNumber: document.getElementById("bpcBarrelNr"),
+        barrelNumber: document.getElementById("bpcBarrelNr")
+    },
+    achievementBoostCalc: {
+        statusText: document.getElementById("textABC"),
+        start: document.getElementById("abcStart"),
+        goal: document.getElementById("abcGoal"),
+        //achievements: document.getElementById("abcAchievements")
     },
     bottom: {
         header: document.getElementById("header"),
         patchNotes: document.getElementById("patchNotes"),
+        currentVersion: document.getElementById("currentVersion")
     }
 }
 
@@ -113,143 +140,24 @@ function determineLastSundayOfMonth(monthToCheck) {
     return 0;
 }
 
-function updateGlobalChallengeTime() {
-    // Status: Is Global Challenge running / when / how long
-
-    let text = "";
-
-    let isGC = (Math.floor((date.getTime() / 1000 / 60 / 60 + (2 - hourDelay)) / 24) - 1) % 2;
-    text = "Today is " + weekdays[serverDate.getDay()] + "."
-        + "<br />Today the Global Challenge is <span style='font-size:32px; color:" + (isGC ? "lightgreen" : "red") + "'>" + (isGC ? "ACTIVE!" : "INACTIVE!") + "</span>"
-        + (isGC ? ("<br />It started " + serverMidnight.getHours() + " hours and " + serverMidnight.getMinutes() + " minutes ago.") :
-        ("<br />It will start in " + (23 - serverMidnight.getHours()) + " hours and " + (59 - serverMidnight.getMinutes()) + " minutes."))
-        + "<br /><br /><br />The server time is " + serverDate.toString().split(" GMT")[0] + " (GMT+" + serverTimeOffset + ")."
-        + "<br />Reset is always at 22:00 GMT+0. (Currently " + (isSummerTime ? "0:00" : "23:00") + " for Germany/the server)";
-
-    ui.globalChallengeStatus.statusText.innerHTML = text;
-}
-
-// combineGainCalc
-function calculateGlobalChallenge() {
-    // Calculator: How many Combine Tokens will I earn
-
-    let combinesEarned = ui.combineGainCalc.progressGlobal.value * ui.combineGainCalc.progressYours.value;
-    let combineMulti = 1;
-
-    if (ui.combineGainCalc.moreTokensLevel.value != "") {
-        combineMulti = 1 + (ui.combineGainCalc.moreTokensLevel.value / 10);
-    }
-    if (ui.combineGainCalc.moreTokensLevel2.value != "") {
-        if (ui.combineGainCalc.moreTokensLevel2.value >= 500) combineMulti = 2 + Math.max(0, Math.floor((ui.combineGainCalc.moreTokensLevel2.value - 1000) / 1000) / 10);
-        else combineMulti = 1;
-    }
-
-    combinesEarned *= combineMulti;
-
-    ui.combineGainCalc.statusText.innerHTML = "You will earn <big>" + Math.floor(combinesEarned) + " <img src='images/combineToken.png' style='width: 32px' />!</big>";
-
-    let sidesSize = 120;
-
-    ui.combineGainCalc.combinesLeft.style.height = "";
-    ui.combineGainCalc.combinesRight.style.height = "";
-    if (ui.combineGainCalc.combinesLeft.clientHeight > sidesSize) sidesSize = ui.combineGainCalc.combinesLeft.clientHeight;
-    if (ui.combineGainCalc.combinesRight.clientHeight > sidesSize) sidesSize = ui.combineGainCalc.combinesRight.clientHeight;
-
-    ui.combineGainCalc.combinesLeft.style.height = sidesSize + "px";
-    ui.combineGainCalc.combinesRight.style.height = sidesSize + "px";
-}
-
-// moreScrapCalc
-function calculateMoreScrap() {
-    // Calculator: What should my More Scrap level be
-
-    let highestScrapEver = ui.moreScrapCalc.highestScrapEver.value != "" ? ui.moreScrapCalc.highestScrapEver.value : -1;
-    let moreGSLevel = ui.moreScrapCalc.moreGSLevel.value != "" ? ui.moreScrapCalc.moreGSLevel.value : -1;
-
-    let a = Math.log10(1.4) / highestScrapEver;
-    let result = ((1 / (2 * a)) * -1) + Math.sqrt(Math.pow(moreGSLevel, 2) + (moreGSLevel / 0.03) + (1 / (4 * Math.pow(a, 2))));
-
-    if (highestScrapEver == -1 || moreGSLevel == -1) ui.moreScrapCalc.statusText.innerHTML = "Please enter your numbers";
-    else ui.moreScrapCalc.statusText.innerHTML = "a: " + a.toFixed(6) + ". <b>Your More Scrap (Book upgrade) should be level " + Math.floor(result) + "!</b>";
-}
-
-// tokenCostCalc
-var adNames = ["automerge", "bricks", "moremagnets", "morescrap", "fasterbarrels", "flu", "morefragments"];
-const adCosts = {
-    "automerge":     [0, 1, 5, 1, 12, 8, 1],
-    "bricks":        [1, 0, 1, 1, 1, 1, 1],
-    "moremagnets":   [5, 1, 0, 1, 8, 5, 1],
-    "morescrap":     [1, 1, 1, 0, 1, 1, 1],
-    "fasterbarrels": [12, 1, 8, 1, 0, 8, 5],
-    "flu":           [8, 1, 5, 1, 8, 0, 1],
-    "morefragments": [1, 1, 1, 1, 5, 1, 0]
-}
-
-function calculateTokenCosts() {
-    let costs = 0;
-    let ad1 = ui.tokenCostCalc.selectedAd1.value;
-    let ad2 = ui.tokenCostCalc.selectedAd2.value;
-
-    if (ad1 == "none" || ad2 == "none") {
-        // one of the two ads is not selected
-        ui.tokenCostCalc.statusText.innerHTML = "(Select the two ads you want to combine!)";
-    }
-    else if (ad1 == ad2) {
-        // you want x100 barrels huh?!
-        ui.tokenCostCalc.statusText.innerHTML = "(Don't select the same boost twice)";
-    }
-    else {
-        // both ARE selected
-        costs = adCosts[ad1][adNames.indexOf(ad2)];
-
-        if (ui.tokenCostCalc.tokenCostAmountOfTokens.value != 0) {
-            let times = Math.floor(ui.tokenCostCalc.tokenCostAmountOfTokens.value / costs);
-            costs = costs * times;
-            ui.tokenCostCalc.statusText.innerHTML = times + " Times (" + costs + " Token" + + (costs > 1 ? "s" : "") + ")";
-        }
-        else {
-            costs *= ui.tokenCostCalc.tokenCostAmountOfAds.value;
-            ui.tokenCostCalc.statusText.innerHTML = costs + " Token" + (costs > 1 ? "s" : "");
-        }
-    }
-}
-
-// barrelProductionCalc
-function calculateBarrelProduction() {
-    let baseProd = ui.barrelProductionCalc.productionBarrelOne.value; // received as a string
-    try {
-        baseProd = new Decimal(baseProd);
-    }
-    catch {
-        ui.barrelProductionCalc.statusText.innerHTML = "Wrong format! Do it like this: 1.567e102 or 1 or 1.749e400";
-        // alert("Wrong format! Do it like this: 1.567e102 or 1 or 1.749e400");
-        return false;
-    }
-
-    let barrelnr = ui.barrelProductionCalc.barrelNumber.value;
-
-    // 3 ^ barrel x base
-    let result = new Decimal(3).pow(barrelnr - 1).mul(baseProd);
-    result = result.mantissa.toString().substr(0, 5) + "e" + result.exponent.toString();
-
-    if (barrelnr == "" || barrelnr < 1) ui.barrelProductionCalc.statusText.innerHTML = "That is not a real barrel!";
-    else if (baseProd.eq(1)) ui.barrelProductionCalc.statusText.innerHTML = "Barrel " + barrelnr + "'s base production is " + result + " Scrap/s.";
-    else ui.barrelProductionCalc.statusText.innerHTML = "Your barrel " + barrelnr + " should produce " + result + " Scrap/s.";
-}
-
 // LOOP
 function loop() {
     updateTime();
 
-    updatePatchNotes();
-
     // UI update
-    updateGlobalChallengeTime();
+    calculateGlobalChallengeTime();
     calculateGlobalChallenge();
     calculateMoreScrap();
     calculateTokenCosts();
     calculateBarrelProduction();
+    calculateAchievementBoost();
 }
 
-ui.bottom.header.innerHTML = "CombCalc " + currentVersion;
-setInterval(loop, 1000 / FPS);
+function init() {
+    ui.bottom.header.innerHTML = "CombCalc " + currentVersion;
+    updatePatchNotes();
+
+    setInterval(loop, 1000 / FPS);
+}
+
+init();
